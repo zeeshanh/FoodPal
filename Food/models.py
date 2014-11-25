55d281dee38f6d283ebe9d43722f9fe777b56a2f
Tamim Jabban <tamimjabban@live.com>
2014-11-25 14:09:10 +0300

    COMMIT 6

--- Food/models.py
+++ Food/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Restaurant(models.Model):
 	name = models.CharField(max_length=200)
-	webpage = models.CharField(max_length=200, null=True)
+	webpage = models.CharField(max_length=200,null=True)
 	def __unicode__(self):
 		return self.name
 		
